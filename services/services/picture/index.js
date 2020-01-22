2969030aacbb3858613075344e3b603962332ed7
alex <thealexanderpohl@gmail.com>
2020-01-22 19:45:27 +0100

    Picure Service

--- services/services/picture/index.js
+++ services/services/picture/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const path = require('path');
+const multer = require('multer')
+const fs = require('fs')
 
 const app = express();
 
 const port = process.env.PORT;
 const stage = process.env.NODE_ENV;
 
-/* loging functions
-*/
+/* 
+ * loging functions
+ */
 // jeden Request
 function logRequest(req,res,next)
 {
@@ -28,13 +31,109 @@ app.use(logError);
  * Hauptapp
  */
 
+// Bilder Ordner
 const dir = path.join(__dirname,'img')
+
+ // Nur Bilder zulassen
+function fileFilter(req,file,cb)
+{
+    switch(file.mimetype)
+    {
+        case "image/gif":
+        case "image/jpeg":
+        case "image/png":
+            cb(null,true);
+            break;
+        default: 
+            cb(new multer.MulterError())
+    }
+}
+
+// Dateinamen festlegen
+const storage = multer.diskStorage({
+    destination: function(req,file,cb)
+    {
+        cb(null,dir);
+    },
+    filename: function(req,file,cb)
+    {
+        var ending = "";
+        switch(file.mimetype)
+        {
+            case "image/gif":
+                ending = ".gif";
+                break;
+            case "image/jpeg":
+                ending = ".jpeg";
+                break;
+            case "image/png":
+                ending = ".png";
+                break;
+        }
+        var filename;
+        do{
+            filename = Math.random().toString(36).substring(2,15)+Math.random().toString(36).substring(2,15);
+        }
+        while(fs.existsSync(path.join(dir,filename+ending)));
+        
+        cb(null,filename+ending);
+    }
+})
+
+
+// Einstellungen für Multer
+const upload = multer(
+    {
+        storage: storage,
+        fileFilter: fileFilter,
+        limits: {
+            fileSize: 2097152
+        }
+    }
+).single('photo');
+
+
+// Dateien erstellen
+app.post('/',
+    function(req,res){       
+        upload(req,res, function(err)
+        {
+            if(err instanceof multer.MulterError)
+            {
+                console.error(err);
+                res.status(400).send("File not allowd");
+            }else if(err)
+            {
+                console.error(err);
+                res.statusStatus(500);
+            }
+            res.send(req.file.filename);
+            console.log("File uploaded: "+req.file.filename);
+        });
+    }
+);
+
+// Dateien löschen
+app.delete('/:name',function(req,res){
+    try{
+        let file = path.join(dir,req.params.name);
+        if(!fs.existsSync(file))
+        {
+            res.sendStatus(404);
+            return;
+        }
+        fs.unlinkSync(file);
+        res.sendStatus(200);
+    }catch(err)
+    {
+        console.error(err);
+        res.sendStatus(500);
+    }
+});
+
+// Images bereitstellen
 app.use(express.static(dir))
-
-
-
 /**
  * Starten
  */
 app.listen(port, () => console.log(`Picture Service (${stage}) Listen On ${port}`))
-
