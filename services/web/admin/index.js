0fed7340b2a9c6f3cc590f4a5139181f957fbe6f
Lukas <lukegraphix@outlook.com>
2020-01-23 13:50:42 +0100

    Init Admin Page

--- services/web/admin/index.js
+++ services/web/admin/index.js
@@ -5,6 +5,7 @@ const app = express();
 const port = process.env.PORT;
 const stage = process.env.NODE_ENV;
 
+const people = require('./people.json');
 /* loging functions
 */
 // jeden Request
@@ -27,12 +28,27 @@ app.use(logError);
  * Hauptapp
  */
 
+ app.set('view engine', 'pug');
+ // serve static files from the `public` folder
+ app.use(express.static(__dirname + '/public'));+
 
-app.get('/', (req, res) => res.send('Hello World!'))
+ app.get('/', (req, res) => {
+   res.render('index', {
+     title: 'Homepage',
+     people: people.profiles
+   });
+ });
+
+ app.get('/profile', (req, res) => {
+   const person = people.profiles.find(p => p.id === req.query.id);
+   res.render('profile', {
+     title: `About ${person.firstname} ${person.lastname}`,
+     person,
+   });
+ });
 
 
 /**
  * Starten
  */
 app.listen(port, () => console.log(`Picture Service (${stage}) Listen On ${port}`))
-
