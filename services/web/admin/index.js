b649bc81900491e638e356133e48c0703b133653
Lukas <lukegraphix@outlook.com>
2020-01-24 20:40:16 +0100

    init error while attempt include bootstrap

--- services/web/admin/index.js
+++ services/web/admin/index.js
@@ -6,6 +6,12 @@ const port = process.env.PORT;
 const stage = process.env.NODE_ENV;
 
 const people = require('./people.json');
+// Versuch bootstrap als node-module einzubinden
+// var jquery = require('jquery');
+// var bootstrap = require('bootstrap');
+// var scss_boot = require('~bootstrap/scss/bootstrap');
+// var scss = require('./scss/app.scss');
+
 /* loging functions
 */
 // jeden Request
@@ -27,25 +33,26 @@ app.use(logError);
 /**
  * Hauptapp
  */
-
- app.set('view engine', 'pug');
- app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
  // serve static files from the `public` folder
- app.use(express.static(__dirname + '/public'));+
+app.use(express.static(__dirname + '/public'));
+// app.use('/jsB', express.static('/node_modules/jquery/dist')); // redirect JS jQuery
+// app.use('/cssB', express.static('/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
  app.get('/', (req, res) => {
    res.render('index', {
-     title: 'Homepage',
+     title: 'Webshop Dashboard',
      people: people.profiles
    });
  });
 
- app.get('/profile', (req, res) => {
-   const person = people.profiles.find(p => p.id === req.query.id);
-   res.render('profile', {
-     title: `About ${person.firstname} ${person.lastname}`,
-     person,
-   });
+   app.get('/profile', (req, res) => {
+     const person = people.profiles.find(p => p.id === req.query.id);
+     res.render('profile', {
+       title: `About ${person.firstname} ${person.lastname}`,
+       person,
+     });
  });
 
 
