2246d9e9debaf95e1b869db1a279d3c24f73a474
addbert <neubert.adrian@googlemail.com>
2020-01-28 19:04:47 +0100

    Subproductschema added

---
+++ services/services/product/subproductschema.js
@@ -0,0 +1,20 @@
+const Mongoose = require('mongoose');
+/*Datenstruktur eines Datensatzes für einen Produktkategoriedatensatz*/
+let spschema = new Mongoose.Schema({
+  subproductid:
+  {
+    type: Number,
+    require: true,
+  },
+  propertys:
+  {
+    type: [String],
+    default: undefined,
+  },
+  amount:
+  {
+    type: Number,
+    default: 0,
+  }
+});
+module.exports = spschema;
