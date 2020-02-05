766abad5a5c92a92b4b37749a28d666374bcd02f
alex <thealexanderpohl@gmail.com>
2020-02-05 18:44:18 +0100

    User: Database Example Data

--- database/user/user_20_data.sql
+++ database/user/user_20_data.sql
@@ -1,4 +1,4 @@
-INSERT INTO userDB.user(firstname ,lastname,mail) VALUES 
+INSERT INTO userDB.user(firstname ,lastname,mail,pword) VALUES 
     ('Admin','Admin','admin@localhost','$2b$08$V9rw7UwUXEH0e2fL.2Ni.u7RwEntJdPQXmx7pUfAncaVjZ0aiROLW'),
     ('Adrian','Neubert','adri.an@gmx.de','$2b$08$V9rw7UwUXEH0e2fL.2Ni.u7RwEntJdPQXmx7pUfAncaVjZ0aiROLW'),
     ('Lukas','Klausnitzer','lukas.k@luke.de','$2b$08$V9rw7UwUXEH0e2fL.2Ni.u7RwEntJdPQXmx7pUfAncaVjZ0aiROLW'),
