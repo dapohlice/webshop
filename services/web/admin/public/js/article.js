7e147881c065ff8483d58dcc2f45f21055829584
Lukas <lukegraphix@outlook.com>
2020-02-19 12:59:24 +0100

    adding error tables for empty responses in group/user/article/category

--- services/web/admin/public/js/article.js
+++ services/web/admin/public/js/article.js
@@ -1,7 +1,7 @@
 // article.js
 // managed all requests and responses for articles
 
-$(function test(){
+$(function (){
 
   // var $article = $('#article');
   var $articles = $('#articles');
@@ -17,10 +17,13 @@ $(function test(){
   var $category = $('#category');
 
   var articlesTemplate = $('#articles-template').html();
-  var articleTemplate = $('#article-template').html();
+  var emptyTemplate = $('#empty-template').html();
 
   function addArticle(article) {
     $articles.append(Mustache.render(articlesTemplate, article));
+  }
+  function addEmpty(empty) {
+    $articles.append(Mustache.render(emptyTemplate, empty));
   }
   function addArticleDetail(article) {
     console.log("addArticleDetail");
@@ -31,10 +34,14 @@ $(function test(){
     type: 'GET',
     url: 'http://localhost:3002/article',
     success: function(articles) {
-      $.each(articles, function(i, article) {
-        addArticle(article);
-        // test();
-      });
+      if ((JSON.stringify(articles) !== JSON.stringify([]))) {
+        $.each(articles, function(i, article) {
+          addArticle(article);
+        });
+      } else {
+        addEmpty(articles);
+      }
+
     },
     error: function() {
       renderErrorTableHTML();
@@ -71,24 +78,48 @@ $(function test(){
     });
   });
 
-  $('.editArticle').on('click', function() {
-    var $tr = $(this).closest('tr');
-    console.log($tr.attr('data-id'));
-    $.ajax({
-      type: 'GET',
-      url: 'http://localhost:3002/article/' + $tr.attr('data-id'),
-      success: function(article) {
-        console.log("success addArticleDetail");
-        addArticleDetail(article);
-      },
-      error: function() {
-        renderErrorTableHTML();
-      }
-    });
-
-  });
-
-
-
 
 });
+
+function edit() {
+  var $articles = $('#articles');
+  var $articleDetailForm = $('#articleDetailForm');
+  var $productid = $('#productid');
+  var $name = $('#name');
+  var $description = $('#description');
+  var $price = $('#price');
+  var $state = $('#state');
+  var $subid = $('#subid');
+  var $property = $('#property');
+  var $amount = $('#amount');
+  var $category = $('#category');
+
+  var articleTemplate = $('#article-template').html();
+  function addArticleDetail(article) {
+    console.log("addArticleDetail");
+    $articles.append(Mustache.render(articleTemplate, article));
+  }
+
+    $('.editArticle').on('click', function() {
+      var $tr = $(this).closest('tr');
+      console.log($tr.attr('data-id'));
+      $.ajax({
+        type: 'GET',
+        url: 'http://localhost:3002/article/' + $tr.attr('data-id'),
+        success: function(article) {
+          console.log("success addArticleDetail");
+          if ((JSON.stringify(article) !== JSON.stringify([]))) {
+            addArticleDetail(article);
+          } else {
+            renderErrorTableHTML();
+          }
+
+        },
+        error: function() {
+          renderErrorTableHTML();
+        }
+      });
+
+    });
+
+}
