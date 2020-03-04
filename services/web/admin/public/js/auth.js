2c7745f7daf0fa6e5b93eaf7dcb8c43cce89d5db
dapohlice <thealexanderpohl@gmail.com>
2020-03-04 17:01:30 +0100

    Bugfixes from Merge

---
+++ services/web/admin/public/js/auth.js
@@ -0,0 +1,35 @@
+function setJwtCookie(auth){
+    console.log(`Auth Cookie ${auth}`);
+    if(auth != null)
+    {
+        var d = new Date();
+        d.setTime(d.getTime() + (3500000));
+        document.cookie = `jwt=${auth};expires=${d.toUTCString()};path=/`;
+    }
+}
+
+function getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length == 2) return parts.pop().split(";").shift();
+  }
+
+function getJwtCookie(){
+    var jwt = getCookie("jwt");
+    return jwt;
+}
+
+function deleteCookie(name){
+    document.cookie = name+"= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+}
+
+function deleteJwt(){
+    deleteCookie("jwt");
+}
+
+document.getElementById("logout").addEventListener('click',function(e)
+{
+    e.preventDefault();
+    deleteJwt();
+    window.open("/","_top");
+})
