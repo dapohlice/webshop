931d02fa5d904fcadaf5c6db832b8a7218c27275
dapohlice <thealexanderpohl@gmail.com>
2020-03-04 23:53:49 +0100

    Admin: Change Status improved

---
+++ services/web/admin/public/js/order.js
@@ -0,0 +1,53 @@
+function setNextStatus() {
+    var url = '';
+    var urlParamID = 0;
+    var status = 0;
+    urlParamID = lastID;
+    status = lastStatus;
+  
+  
+    let req = SimpleRequest.PATCH(ORDER_SERVICE,'order/'+urlParamID)
+    .onSuccess(function(){
+      setNewStatus = true;
+      setOrderDetails = false;
+      lastID = 0;
+      lastStatus = 0;
+    })
+    .onUnauthorized(function(){
+      showStatusError("You are not allwed to change this status.");
+    })
+    .onFailure(function(){
+      showStatusError("It is not possible to change this status.");
+    })
+    .onError(function(){
+      showStatusError("An network Error occurred.");
+    });
+  
+  
+    let message = document.getElementById('nextStatusMessage').value;
+    if(message !== '' || message !== undefined)
+    {
+      let data = {
+        info: message
+      }
+      req.addJson(data);
+    }
+    req.send();
+  
+  }
+
+document.getElementById('acceptStatusButton').addEventListener('click',function(e){
+    setNextStatus();
+    $('#changeOrderStatusModal').modal('hide');
+});
+
+
+document.getElementById('changeStatusButton').addEventListener('click',function(e)
+{
+    e.preventDefault();
+    document.getElementById('nextStatusMessage').value = "";
+    $('#changeOrderStatusModal').modal('show');
+
+});
+
+
--- services/web/admin/public/js/request.js
+++ services/web/admin/public/js/request.js
@@ -326,43 +326,7 @@ function putGroup() {
     console.log(json);
   }
 }
-function setNextStatus() {
-  var url = '';
-  var urlParamID = 0;
-  var status = 0;
-  urlParamID = lastID;
-  status = lastStatus;
 
-
-  let req = SimpleRequest.PATCH(ORDER_SERVICE,'order/'+urlParamID)
-  .onSuccess(function(){
-    setNewStatus = true;
-    setOrderDetails = false;
-    lastID = 0;
-    lastStatus = 0;
-  })
-  .onUnauthorized(function(){
-    showStatusError("You are not allwed to change this status.");
-  })
-  .onFailure(function(){
-    showStatusError("It is not possible to change this status.");
-  })
-  .onError(function(){
-    showStatusError("An network Error occurred.");
-  });
-
-
-  let message = document.getElementById('nextStatusMessage').value;
-  if(message !== '' || message !== undefined)
-  {
-    let data = {
-      info: message
-    }
-    req.addJson(data);
-  }
-  req.send();
-
-}
 function getOrderDetails(id) {
   setDetailsFalse();
   var url = '';
