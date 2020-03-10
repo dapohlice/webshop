b6eca1971f1ea42e246ae9577c27f053050d5b99
addbert <neubert.adrian@googlemail.com>
2020-03-10 12:53:20 +0100

    Deactivate MongoID for Propertys-SubDocument

--- services/services/product/database/schemas/SubSchema.js
+++ services/services/product/database/schemas/SubSchema.js
@@ -1,8 +1,8 @@
 const Mongoose = require('mongoose');
 /*Datenstruktur eines Datensatzes für einen Produktkategoriedatensatz*/
 let spschema = new Mongoose.Schema({
-  subid: { type: Number, required: true, },
-  property: { type: String, required: true,},
-  amount: { type: Number, required: true, default: 0, }
-});
+  subid: { type: Number, required: true, unique: true},
+  property: { type: String, required: true},
+  amount: { type: Number, required: true, default: 0,}
+},{_id: false});
 module.exports = spschema;
